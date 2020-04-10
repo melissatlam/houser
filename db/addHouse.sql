49685e5df3a67aca96c686eb376f440affe91780
Melissa <Melissalammm@gmail.com>
2020-04-10 16:44:31 -0500

    done

--- db/addHouse.sql
+++ db/addHouse.sql
@@ -1,1 +1,1 @@
-INSERT INTO houses (name, address, city, state, zipcode, image, mortgage) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8);
+INSERT INTO houses (name, address, city, state, zipcode, image, mortgage, rent) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8);
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -8,6 +8,8 @@ export default class Dashboard extends Component {
         this.state = {
             houses: []
         }
+        this.componentDidMount = this.componentDidMount.bind(this);
+  
     }
     componentDidMount() {
         this.getHouses()
@@ -24,26 +26,25 @@ export default class Dashboard extends Component {
             })
     }
 
-    addHouses= newArr => {
+    addHouse= newArr => {
         this.setState({ houses: newArr })
     }
 
-    // removeHouses = () => {
-    //     axios.delete(`/api/houser/${this.props.val.id}`)
-    //     .then(response => {
-    //         this.props.houses(response.data);
-    //         this.props.getHouses();
-    //     })
-    // }
+    removeHouse = () => {
+        axios.delete(`/api/houses/${this.props.val.id}`)
+        .then(response => {
+            this.props.houses(response.data);
+            this.props.getHouses();
+        })
+    }
 
 
     render() {
-        console.log(this.state.houses)
+        console.log(this.state.houses);
         let mappedHouses = this.state.houses.map((val, index) => {
-            return (
-                <House val={val} getHouses={this.getHouses}/>
-            )
-        })
+         return <House val={val} addHouse={this.addHouse} />;
+            });
+
         return (
             <div>
                 <h1>Dashboard</h1>
@@ -52,7 +53,7 @@ export default class Dashboard extends Component {
                 
             
             <div>
-                {/* <button onClick={() => this.handleClickAdd()}>Add New Property</button> */}
+                <button onClick={() => this.handleClickAdd()}>Add New Property</button>
             </div></div>
         )
     }
